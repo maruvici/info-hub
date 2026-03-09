ca1e05dff8c09e89ac1ce5f113bd5c549f9a4fee
mav <251095376+mavi-ssi@users.noreply.github.com>
2026-03-09 09:38:23 +0800

    fix: Update logo to change with display

--- info-hub/src/app/(platform)/layout.tsx
+++ info-hub/src/app/(platform)/layout.tsx
@@ -39,7 +39,7 @@ export default async function PlatformLayout({ children }: { children: React.Rea
                 maskSize: 'contain'
               }}
             />
-            <span className="text-gradient after:content-['SSInfoHub'] md:after:content-['SSI Info Hub']"></span>
+            <span className="text-gradient after:content-[var(--content)] [--content:'SSInfoHub'] md:[--content:'SSI_Info_Hub']"></span>
           </Link>
 
           {/* Search - Takes up all available middle space */}
